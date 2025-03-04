fe324c3e889415bb234da81f1cc456937ae94d06
Christopher Brownlie <13515086+cabrownlie@users.noreply.github.com>
2025-03-04 13:46:22 +0000

    Remove unnecessary comments and clean up main module documentation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-//! This is the main module of the "random-shader-window" project.
-//!
-//! It contains the main entry point of the application and the `App` struct, which represents the application itself.
-//! The `App` struct handles window setup, rendering, and event handling.
-//! I'm just using this as a playground to learn how to use wgsl, so it's not really meant to be a useful application.
-//!
-//! Alot of the render pipeline stuff is using default where possible to keep things simple so
-//! I can focus on the shader stuff. It'll probably be a good idea to go back and tweak some of that stuff later
-//! to get it to work on other platforms, but I'm not really worried about that right now.
-
 use winit::{
 	event::{Event, WindowEvent},
 	event_loop::EventLoop,
@@ -51,8 +41,6 @@ async fn main() {
 						x: position.x as f32,
 						y: position.y as f32,
 					});
-					// HACK: Force a redraw, because I can't figure out how to cleanly call
-					// [window.request_redraw()] without the borrow checker complaining.
 					app.render();
 				}
 				WindowEvent::CloseRequested => {
