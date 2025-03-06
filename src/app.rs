6953b8e6326b4f9e8dba135b463b587d87bdfaf2
Christopher Brownlie <13515086+cabrownlie@users.noreply.github.com>
2025-03-06 16:47:22 +0100

    Fix broken merge

--- src/app.rs
+++ src/app.rs
@@ -1,66 +1,53 @@
+use std::sync::Arc;
+
 use wgpu::{
-	include_wgsl, BindGroupDescriptor, BindGroupEntry, BindGroupLayoutDescriptor,
-	BindGroupLayoutEntry, BindingResource, BindingType, BufferBindingType, BufferDescriptor,
-	BufferUsages, ColorTargetState, CommandEncoderDescriptor, DeviceDescriptor, Features,
-	FragmentState, Instance, Limits, MultisampleState, PipelineLayoutDescriptor, PowerPreference,
-	PrimitiveState, RenderPassColorAttachment, RenderPassDescriptor, RenderPipelineDescriptor,
-	RequestAdapterOptions, ShaderStages, TextureViewDescriptor, VertexState,
+	include_wgsl, BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingType, BufferBindingType,
+	BufferDescriptor, BufferUsages, PipelineLayoutDescriptor, ShaderStages,
 };
-use winit::window::Window;
+use winit::{
+	application::ApplicationHandler,
+	event::WindowEvent,
+	event_loop::ActiveEventLoop,
+	window::{Window, WindowId},
+};
 
-use crate::MousePosition;
+pub struct App {
+	window: Arc<Window>,
 
-// impl bytemuck::Pod and bytemuck::Zeroable so we can use it as a buffer
-// I'm not sure if this is the best way to do this, but it works.
-unsafe impl bytemuck::Pod for MousePosition {}
-unsafe impl bytemuck::Zeroable for MousePosition {}
-
-pub struct App<'a> {
-	// Render stuff
-	surface: wgpu::Surface<'a>,
-	adapter: wgpu::Adapter,
-	queue: wgpu::Queue,
+	// Render
 	device: wgpu::Device,
-	render_pipeline: wgpu::RenderPipeline,
+	queue: wgpu::Queue,
+	size: winit::dpi::PhysicalSize<u32>,
+	surface: wgpu::Surface<'static>,
+	surface_format: wgpu::TextureFormat,
+	pipeline: wgpu::RenderPipeline,
 
-	// Buffer stuff
+	// Buffer
 	buffer: wgpu::Buffer,
 	bind_group: wgpu::BindGroup,
-	pub mouse_position: Option<MousePosition>,
+	mouse_position: Option<MousePosition>,
 }
 
-impl App<'_> {
-	pub async fn new(window: Window) -> Self {
-		// Grab the window size now, so we can use it to size our surface.
-		// We need it here because we won't have a borrow on the window later.
-		let size = window.inner_size();
-
-		// Setting up access to the GPU and the surface to render too.
-		let instance = Instance::default();
-		let surface = instance.create_surface(window).expect("Create surface");
+impl App {
+	pub async fn new(window: Arc<Window>) -> Self {
+		let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
 		let adapter = instance
-			.request_adapter(&RequestAdapterOptions {
-				power_preference: PowerPreference::default(),
-				force_fallback_adapter: false,
-				compatible_surface: Some(&surface),
-			})
+			.request_adapter(&wgpu::RequestAdapterOptions::default())
 			.await
-			.expect("Request adapter");
+			.unwrap();
 		let (device, queue) = adapter
 			.request_device(
-				&DeviceDescriptor {
-					label: None,
-					required_features: Features::default(),
-					required_limits: Limits::default(),
-				},
-				None,
+				&wgpu::DeviceDescriptor::default(),
+				None, // Trace path
 			)
 			.await
-			.expect("Request device");
-		let surface_config = surface
-			.get_default_config(&adapter, size.width, size.height)
-			.expect("Get default surface config");
-		surface.configure(&device, &surface_config);
+			.unwrap();
+
+		let size = window.inner_size();
+
+		let surface = instance.create_surface(window.clone()).unwrap();
+		let cap = surface.get_capabilities(&adapter);
+		let surface_format = cap.formats[0];
 
 		// Set up a buffer to store mouse position
 		let buffer = device.create_buffer(&BufferDescriptor {
@@ -82,81 +69,124 @@ impl App<'_> {
 			}],
 			label: None,
 		});
-		let bind_group = device.create_bind_group(&BindGroupDescriptor {
-			layout: &bind_group_layout,
-			entries: &[BindGroupEntry {
-				binding: 0,
-				resource: BindingResource::Buffer(buffer.as_entire_buffer_binding()),
-			}],
+
+		let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
 			label: None,
+			layout: &bind_group_layout,
+			entries: &[wgpu::BindGroupEntry {
+				binding: 0,
+				resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+					buffer: &buffer,
+					offset: 0,
+					size: None,
+				}),
+			}],
 		});
+
+		let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
+
 		let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
 			bind_group_layouts: &[&bind_group_layout],
 			..Default::default()
 		});
 
-		// Set up the render pipeline
-		let shader = device.create_shader_module(include_wgsl!("../assets/shader.wgsl"));
-		let render_pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
+		let swapchain_capabilities = surface.get_capabilities(&adapter);
+		let swapchain_format = swapchain_capabilities.formats[0];
+
+		let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
 			label: None,
 			layout: Some(&pipeline_layout),
-			vertex: VertexState {
+			vertex: wgpu::VertexState {
 				module: &shader,
-				entry_point: "vertex",
+				entry_point: Some("vertex"),
+				compilation_options: Default::default(),
 				buffers: &[],
 			},
-			primitive: PrimitiveState::default(),
-			depth_stencil: None,
-			multisample: MultisampleState::default(),
-			fragment: Some(FragmentState {
+			fragment: Some(wgpu::FragmentState {
 				module: &shader,
-				entry_point: "fragment",
-				targets: &[Some(ColorTargetState {
-					format: surface_config.format,
-					blend: None,
-					write_mask: Default::default(),
-				})],
+				entry_point: Some("fragment"),
+				compilation_options: Default::default(),
+				targets: &[Some(swapchain_format.into())],
 			}),
+			primitive: wgpu::PrimitiveState::default(),
+			depth_stencil: None,
+			multisample: wgpu::MultisampleState::default(),
 			multiview: None,
+			cache: None,
 		});
 
-		Self {
-			surface,
-			adapter,
-			queue,
+		let state = App {
+			window,
 			device,
-			render_pipeline,
+			queue,
+			size,
+			surface,
+			surface_format,
+			pipeline,
 			buffer,
 			bind_group,
 			mouse_position: None,
-		}
+		};
+
+		// Configure surface for the first time
+		state.configure_surface();
+
+		state
 	}
 
-	pub fn render(&self) {
+	fn configure_surface(&self) {
+		let surface_config = wgpu::SurfaceConfiguration {
+			usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+			format: self.surface_format,
+			// Request compatibility with the sRGB-format texture view we‘re going to create later.
+			view_formats: vec![self.surface_format.add_srgb_suffix()],
+			alpha_mode: wgpu::CompositeAlphaMode::Auto,
+			width: self.size.width,
+			height: self.size.height,
+			desired_maximum_frame_latency: 2,
+			present_mode: wgpu::PresentMode::AutoVsync,
+		};
+		self.surface.configure(&self.device, &surface_config);
+	}
+
+	fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+		self.size = new_size;
+
+		// reconfigure the surface
+		self.configure_surface();
+	}
+
+	fn render(&mut self) {
+		// Create texture view
 		let surface_texture = self
 			.surface
 			.get_current_texture()
-			.expect("Get current texture");
+			.expect("failed to acquire next swapchain texture");
 		let texture_view = surface_texture
 			.texture
-			.create_view(&TextureViewDescriptor::default());
+			.create_view(&wgpu::TextureViewDescriptor::default());
 
-		let mut command_encoder = self
-			.device
-			.create_command_encoder(&CommandEncoderDescriptor::default());
-		{
-			let mut render_pass = command_encoder.begin_render_pass(&RenderPassDescriptor {
-				color_attachments: &[Some(RenderPassColorAttachment {
-					view: &texture_view,
-					resolve_target: None,
-					ops: Default::default(),
-				})],
-				..Default::default()
-			});
-			render_pass.set_pipeline(&self.render_pipeline);
-			render_pass.set_bind_group(0, &self.bind_group, &[]);
-			render_pass.draw(0..6, 0..1);
-		}
+		// Renders screen
+		let mut encoder = self.device.create_command_encoder(&Default::default());
+		// Create the renderpass which will clear the screen.
+		let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+			label: None,
+			color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+				view: &texture_view,
+				resolve_target: None,
+				ops: Default::default(),
+			})],
+			depth_stencil_attachment: None,
+			timestamp_writes: None,
+			occlusion_query_set: None,
+		});
+
+		render_pass.set_pipeline(&self.pipeline);
+		render_pass.set_bind_group(0, &self.bind_group, &[]);
+		render_pass.draw(0..6, 0..1);
+
+		// End the renderpass.
+		drop(render_pass);
 
 		// Copy the mouse position into the buffer if we have one
 		if let Some(mp) = self.mouse_position {
@@ -164,17 +194,57 @@ impl App<'_> {
 				.write_buffer(&self.buffer, 0, bytemuck::cast_slice(&[mp]))
 		}
 
-		self.queue.submit(Some(command_encoder.finish()));
+		// Submit the command in the queue to execute
+		self.queue.submit([encoder.finish()]);
+		self.window.pre_present_notify();
 		surface_texture.present();
 	}
+}
 
-	pub fn resize(&mut self, size: winit::dpi::PhysicalSize<u32>) {
-		self.surface.configure(
-			&self.device,
-			&self
-				.surface
-				.get_default_config(&self.adapter, size.width, size.height)
-				.expect("Get default surface config"),
+impl ApplicationHandler for App {
+	fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+		self.window = Arc::new(
+			event_loop
+				.create_window(Window::default_attributes())
+				.unwrap(),
 		);
 	}
+
+	fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+		match event {
+			WindowEvent::CloseRequested => {
+				event_loop.exit();
+			}
+			WindowEvent::CursorMoved {
+				device_id: _,
+				position,
+			} => {
+				self.mouse_position = Some(MousePosition {
+					x: position.x as f32,
+					y: position.y as f32,
+				});
+				self.render();
+			}
+			WindowEvent::RedrawRequested => {
+				self.render();
+			}
+			WindowEvent::Resized(new_size) => {
+				self.resize(new_size);
+			}
+			_ => (),
+		}
+	}
 }
+
+// This is the struct that we'll use to pass the mouse position to the shader.
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct MousePosition {
+	x: f32,
+	y: f32,
+}
+
+// impl bytemuck::Pod and bytemuck::Zeroable so we can use it as a buffer
+// I'm not sure if this is the best way to do this, but it works.
+unsafe impl bytemuck::Pod for MousePosition {}
+unsafe impl bytemuck::Zeroable for MousePosition {}
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,28 @@
-use winit::{
-	event::{Event, WindowEvent},
-	event_loop::EventLoop,
-	window::WindowBuilder,
-};
+use std::sync::Arc;
 
-mod app;
+use random_shader_window::app::App;
+use winit::{event_loop::EventLoop, window::Window};
 
-#[tokio::main]
-async fn main() {
-	// Setup windowing
-	let event_loop = EventLoop::new().expect("New event loop");
-	let window = WindowBuilder::new()
-		.with_title("Random Shader Window")
-		.build(&event_loop)
-		.expect("New window");
+fn main() {
+	let event_loop = EventLoop::new().unwrap();
 
-	// Setup the app
-	let mut app = app::App::new(window).await;
+	let window = Arc::new(
+		event_loop
+			.create_window(Window::default_attributes())
+			.unwrap(),
+	);
 
-	// Run the event loop
-	let _ = event_loop.run(|event, elwt| {
-		if let Event::WindowEvent {
-			window_id: _,
-			event,
-		} = event
-		{
-			match event {
-				WindowEvent::Resized(size) => {
-					app.resize(size);
-				}
-				WindowEvent::RedrawRequested => {
-					app.render();
-				}
-				WindowEvent::CursorMoved {
-					device_id: _,
-					position,
-				} => {
-					// Pass the mouse position to the app
-					app.mouse_position = Some(MousePosition {
-						x: position.x as f32,
-						y: position.y as f32,
-					});
-					app.render();
-				}
-				WindowEvent::CloseRequested => {
-					elwt.exit();
-				}
-				_ => {}
-			}
-		}
-	});
+	#[cfg(not(target_arch = "wasm32"))]
+	{
+		pollster::block_on(run(event_loop, window));
+	}
+	#[cfg(target_arch = "wasm32")]
+	{
+		wasm_bindgen_futures::spawn_local(run(event_loop, window));
+	}
 }
 
-// This is the struct that we'll use to pass the mouse position to the shader.
-#[repr(C)]
-#[derive(Copy, Clone)]
-struct MousePosition {
-	x: f32,
-	y: f32,
+async fn run(event_loop: EventLoop<()>, window: Arc<Window>) {
+	let mut app = pollster::block_on(App::new(window));
+	let _ = event_loop.run_app(&mut app);
 }
